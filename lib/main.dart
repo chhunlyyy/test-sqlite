ac3a9a62721b15ff35461006cb373578fe0bbdb5
chhunly <60867999+chhunlyyy@users.noreply.github.com>
2022-08-24 22:59:57 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:test_sqlite/sqlite_helper.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  void _init() {
+    SqLiteHelper sqLiteHelper = SqLiteHelper();
+    const String dbName = 'demo.db';
+    const String creatTableQuery = 'CREATE TABLE Test (id INTEGER PRIMARY KEY, name TEXT, value INTEGER, num REAL)';
+    const String insertValueQuery = 'INSERT INTO Test(name, value, num) VALUES("some name", 1234, 456.789)';
+    const String getValueQuery = 'SELECT * FROM Test';
+
+    Future.delayed(Duration.zero, () async {
+      // insert data
+      try {
+        await sqLiteHelper.createTable(dbName, creatTableQuery).then((db) async {
+          await sqLiteHelper.insertRecord(db, insertValueQuery);
+        });
+      } catch (e) {
+        print(e);
+      }
+
+      // get data
+
+      try {
+        String path = await sqLiteHelper.getPath(dbName);
+
+        await openDatabase(path).then((db) async {
+          await sqLiteHelper.getRecord(db, getValueQuery).then((value) {
+            print(value);
+            sqLiteHelper.closeDatabase(db);
+          });
+        });
+      } catch (e) {
+        print(e);
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    _init();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold(body: Container()));
+  }
+}
---
+++ lib/sqlite_helper.dart
@@ -0,0 +1,33 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class SqLiteHelper {
+  Future<String> getPath(String dbName) async {
+    var databasesPath = await getDatabasesPath();
+    return join(databasesPath, dbName);
+  }
+
+  Future<Database> createTable(String path, String rawQuery) async {
+    return await openDatabase(path, version: 1, onCreate: (Database db, int version) async {
+      await db.execute(rawQuery);
+    });
+  }
+
+  Future<void> insertRecord(Database db, String rawInsertQuery) async {
+    await db.transaction((txn) async {
+      await txn.rawInsert(rawInsertQuery);
+    });
+  }
+
+  Future<List<Map>> getRecord(Database db, String getRecordQuery) async {
+    return await db.rawQuery(getRecordQuery);
+  }
+
+  Future<void> deleteRecord(Database db, String deleteRawQuery) async {
+    await db.delete(deleteRawQuery);
+  }
+
+  Future<void> closeDatabase(Database db) async {
+    await db.close();
+  }
+}
